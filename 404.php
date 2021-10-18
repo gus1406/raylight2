c1a5c0079daf7accccb658669afba23ccead3446
gus1406 <gus1406.bisnis@gmail.com>
2021-10-18 16:26:43 +0700

    desain awal

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'; ?>
+
+<main class="main-404" id="Main_404">
+	
+	<div class="main-404-container">
+		<h1 class="main-404-title">404</h1>
+		<p class="main-404-desc">Maaf, halaman yang anda cari tidak ditemukan</p>
+		<p class="main-404-home-link">Kembali ke <a href="#">Beranda</a></p>
+	</div><!-- .main-404-container -->
+
+</main><!-- .main-404 -->
+
+<?php include 'footer.php'; ?>
